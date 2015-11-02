01f5ee0272f65db547706fec2476c55a3ec27805
Sosuke Masui <esmasui@gmail.com>
2015-11-02 15:25:59 +0900

    Fixed; javadoc generation error.

--- library/src/main/java/com/uphyca/galette/HitInterceptor.java
+++ library/src/main/java/com/uphyca/galette/HitInterceptor.java
@@ -17,64 +17,28 @@ public interface HitInterceptor {
 
     interface HitFacade<T extends HitFacade> {
 
-        /**
-         * @see com.google.android.gms.analytics.HitBuilders.HitBuilder#setCustomDimension(int, String)
-         */
         T setCustomDimension(int index, String dimension);
 
-        /**
-         * @see com.google.android.gms.analytics.HitBuilders.HitBuilder#setAll(Map)
-         */
         T setAll(Map<String, String> params);
 
-        /**
-         * @see com.google.android.gms.analytics.HitBuilders.HitBuilder#setPromotionAction(String)
-         */
         T setPromotionAction(String action);
 
-        /**
-         * @see com.google.android.gms.analytics.HitBuilders.HitBuilder#setProductAction(ProductAction)
-         */
         T setProductAction(ProductAction action);
 
-        /**
-         * @see com.google.android.gms.analytics.HitBuilders.HitBuilder#setNewSession()
-         */
         T setNewSession();
 
-        /**
-         * @see com.google.android.gms.analytics.HitBuilders.HitBuilder#addPromotion(Promotion)
-         */
         T addPromotion(Promotion promotion);
 
-        /**
-         * @see com.google.android.gms.analytics.HitBuilders.HitBuilder#setCustomMetric(int, float)
-         */
         T setCustomMetric(int index, float metric);
 
-        /**
-         * @see com.google.android.gms.analytics.HitBuilders.HitBuilder#setCampaignParamsFromUrl(String)
-         */
         T setCampaignParamsFromUrl(String utmParams);
 
-        /**
-         * @see com.google.android.gms.analytics.HitBuilders.HitBuilder#set(String, String)
-         */
         T set(String paramName, String paramValue);
 
-        /**
-         * @see com.google.android.gms.analytics.HitBuilders.HitBuilder#addProduct(Product)
-         */
         T addProduct(Product product);
 
-        /**
-         * @see com.google.android.gms.analytics.HitBuilders.HitBuilder#setNonInteraction(boolean)
-         */
         T setNonInteraction(boolean nonInteraction);
 
-        /**
-         * @see com.google.android.gms.analytics.HitBuilders.HitBuilder#addImpression(Product, String)
-         */
         T addImpression(Product product, String impressionList);
     }
 
